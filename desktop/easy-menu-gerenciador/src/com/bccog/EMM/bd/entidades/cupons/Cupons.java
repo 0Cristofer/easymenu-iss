6a9c2a9fae9aaf9eb7beae8bf47a0579608661a8
guilhermequental <guilherme.quental97@gmail.com>
2017-11-23 21:42:15 -0200

    Implementada a classe Cupons e sua controladora

--- desktop/easy-menu-gerenciador/src/com/bccog/EMM/bd/entidades/cupons/Cupons.java
+++ desktop/easy-menu-gerenciador/src/com/bccog/EMM/bd/entidades/cupons/Cupons.java
@@ -2,10 +2,6 @@ package com.bccog.EMM.bd.entidades.cupons;
     import com.bccog.EMM.bd.EntidadeBasica;
     import com.google.gson.annotations.SerializedName;
 
-    import java.util.ArrayList;
-    import java.util.List;
-
-
 
 /**
  * Classe Cupons
@@ -14,4 +10,42 @@ package com.bccog.EMM.bd.entidades.cupons;
  */
 
 public class Cupons extends EntidadeBasica{
+
+    @SerializedName("nome")
+    private String nome_;
+    @SerializedName("timestamp")
+    private long timestamp_;
+    @SerializedName("valor")
+    private float valor_;
+
+    public Cupons (String nome, long timestamp, float valor){
+        this.nome_ = nome;
+        this.timestamp_ = timestamp;
+        this.valor_ = valor;
+
+    }
+
+    public void setNome_(String nome_) {
+        this.nome_ = nome_;
+    }
+
+    public void setTimestamp_(long timestamp_) {
+        this.timestamp_ = timestamp_;
+    }
+
+    public void setValor_(float valor_) {
+        this.valor_ = valor_;
+    }
+
+    public String getNome_() {
+        return nome_;
+    }
+
+    public long getTimestamp_() {
+        return timestamp_;
+    }
+
+    public float getValor_() {
+        return valor_;
+    }
 }
